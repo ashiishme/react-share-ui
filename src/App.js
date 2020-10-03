226d4d78adb0ef579489946dbb20aded685097c5
n1rjal <nirjalpaudel54312@gmail.com>
2020-10-03 14:10:49 +0545

    Fixed charts and added Tooltips labels

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import React from "react";
-import List from "./components/List";
-import Chart from "./components/Chart";
-import { ShareContext } from "./hooks/useShareContext";
-import useAsyncTask from "./hooks/useAsyncTask";
+import React from 'react';
+import List from './components/List';
+import Chart from './components/Chart';
+import { ShareContext } from './hooks/useShareContext';
+import useAsyncTask from './hooks/useAsyncTask';
 
 export default function App() {
-  const response = useAsyncTask('https://ashiish.me/idart/data/shares.json');
-  return (
-    <div className="app container">
-      <div className="share-data">
-        <ul className="scrip-list">
-          {response?.shares.map((item) => 
-          <ShareContext.Provider value={item} key={item.id}>
-            <List />
-          </ShareContext.Provider>
-          )}
-        </ul>
-      </div>
-      <div className="share-visual">
-        <div className="share-portfolio">
-          <h3>Portfolio</h3>
-          <span>Asset-wise</span>
+    const response = useAsyncTask('https://ashiish.me/idart/data/shares.json');
+    return (
+        <div className="app container">
+            <div className="share-data">
+                <ul className="scrip-list">
+                    {response?.shares.map((item) => (
+                        <ShareContext.Provider value={item} key={item.id}>
+                            <List />
+                        </ShareContext.Provider>
+                    ))}
+                </ul>
+            </div>
+            <div className="share-visual">
+                <div className="share-portfolio">
+                    <h3>Portfolio</h3>
+                </div>
+                <Chart />
+            </div>
         </div>
-        <Chart/>
-      </div>
-    </div>
-  );
+    );
 }
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,35 +1,68 @@
-import React from "react";
-import { PieChart, Pie, Cell, Legend } from 'recharts';
+import React from 'react';
+import { PieChart, Pie, Cell, Legend, Tooltip } from 'recharts';
 
 const data = [
-    { name: 'Mutual Funds', value: 70 },
-    { name: 'ETFs', value: 30 }
+    {
+        name: 'Mutual Funds',
+        value: 70,
+        url: 'https://economictimes.indiatimes.com/mf/mf-news',
+    },
+    {
+        name: 'ETFs',
+        value: 30,
+        url: 'https://www.marketwatch.com/investing/etf',
+    },
 ];
-  
+
+//open Google Search
+const openGS = (Data) => {
+    var name = Data.value;
+    var entry = data.find((elem) => elem.name === name);
+    window.open(entry.url, '_blank');
+};
+
 const COLORS = ['#03A9F4', '#795548'];
 
 const Chart = () => {
     return (
         <>
-        <PieChart width={350} height={300}>
-            <Pie
-            data={data}
-            cx={100}
-            cy={150}
-            innerRadius={60}
-            outerRadius={80}
-            fill="#8884d8"
-            paddingAngle={0}
-            dataKey="value"
-            >
-            {
-                data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
-            }
-            </Pie>
-            <Legend iconSize={10} width={140} height={40} layout='vertical' verticalAlign='middle' align="right" />
-        </PieChart>
+            <PieChart width={250} height={500}>
+                <Pie
+                    data={data}
+                    cx={90}
+                    cy={130}
+                    innerRadius={60}
+                    outerRadius={80}
+                    fill="#8884d8"
+                    paddingAngle={2}
+                    dataKey="value"
+                >
+                    {data.map((entry, index) => (
+                        <Cell
+                            data-name={entry}
+                            key={`cell-${index}`}
+                            fill={COLORS[index % COLORS.length]}
+                        />
+                    ))}
+                </Pie>
+
+                <Legend
+                    iconSize={10}
+                    width={140}
+                    onClick={openGS}
+                    wrapperStyle={{
+                        paddingLeft: '20px',
+                    }}
+                    height={10}
+                    iconType="circle"
+                    layout="horizontal"
+                    verticalAlign="middle"
+                    align="left"
+                />
+                <Tooltip cursor={{ stroke: 'red', strokeWidth: 2 }} />
+            </PieChart>
         </>
     );
-}
+};
 
 export default Chart;
